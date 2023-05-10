3c9c510e179c681af2038b3c8b73b9ff3af03af7
Chet Gray <Chet.Gray@waystar.com>
2023-05-10 16:14:04 -0400

    Merged PR 33541: ✨ feat: WPF: Create Movie Application
    - 📝 docs: add requirements from user story
    - 🎉 feat: begin WPF app project
    - 💄 feat: lay out movie entry form
    - 💄 refactor: use `TextBlock`s instead of `Label`s
    - ✨ feat: show message box on submit
    - ✨ feat: clear fiels after closing submit message box
    - ✨ feat: require all fields, display warning if any empty
    - 🚚 refactor: rename `MovieEntryWindow` to `MainWindow`
    
    Related work items: #212017

---
+++ Movies.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Movies.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (UIElement child in FormLayout.Children)
+            {
+                if (
+                    (child is TextBox textBox && string.IsNullOrWhiteSpace(textBox.Text))
+                    || (child is ComboBox comboBox && comboBox.SelectedIndex == -1)
+                )
+                {
+                    MessageBox.Show("All fields are required.");
+                    return;
+                }
+            }
+
+            MessageBox.Show(
+                $"Entry for Movie {TitleInput.Text} from {ReleaseYearInput.Text} accepted."
+            );
+            foreach (UIElement child in FormLayout.Children)
+            {
+                switch (child)
+                {
+                    case TextBox textBox:
+                        textBox.Text = string.Empty;
+                        break;
+                    case ComboBox comboBox:
+                        comboBox.SelectedIndex = -1;
+                        break;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 Movies.WpfApp
 =============
+
+User Story #212017: WPF: Create Movie Application
+-------------------------------------------------
+
+> Create a simple WPF "Movie" application that does the following:
+>
+> - [x] Presents a form for the user to enter movie information.
+> - [x] Information entered should be:
+>   - Movie Title
+>   - Genre (Action / Horror / RomCom / etc.)
+>   - Director
+>   - Year Released
+>   - Average Star Rating (0 - 5)
+> - [x] It should have a Submit button.
+> - [x] When the user clicks Submit, send a message stating, "Entry for
+>   Movie (Movie Title) from (Year Released) accepted."
+> - [x] After that, clear all the fields so the user can enter another
+>   movie.
